a47699bce2b70ef1c6a31b7b76baa2d932835991
BlasTobares <163307394+BlasTobares@users.noreply.github.com>
2024-04-03 12:35:12 -0300

    primeros cambios

---
+++ src/tp5_lab_i/TP5_Lab_I.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp5_lab_i;
+
+/**
+ *
+ * @author Blas
+ */
+public class TP5_Lab_I {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
