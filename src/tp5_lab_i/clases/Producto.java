a914ecc652ed8dd261fad37c7dfba2ad15effe9a
ArielAricoma <ariel16ary@gmail.com>
2024-04-05 11:06:03 -0300

    primer paso
    er

---
+++ src/tp5_lab_i/clases/Producto.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp5_lab_i.clases;
+
+/**
+ *
+ * @author SrPanda
+ */
+public class Producto {
+    private String nombre;
+    private double precio;
+    private String categoria;
+
+    public Producto() {
+    }
+    
+    public Producto(String nombre, double precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+}
